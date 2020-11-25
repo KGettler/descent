440a6ac10daf9d25ef8fe4cf83dce28c0d549ea4
KGettler <73446927+KGettler@users.noreply.github.com>
2020-11-25 18:22:39 -0500

    Descent v2 - Wall Detection (a real maze!!!)
    While the player was prevented from leaving the maze, there were no internal walls to restrict movement. Now, internal walls are represented in two matrices (wallH and wallV). When the player moves, the game checks if there is a wall based on the players position and direction. Internal walls are also represented graphically.
    Need to add -  hide walls not seen by player, how to adapt system to larger room sizes (so as not to clutter screen), maze randomization (?)
    Scrapped ideas - first person: this is too difficult to implement, due to the complexity of the necessary code and my lack of ASCII art skills. The UI will only consist of the maze map, with potentially a separate menu for combat(?)

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# for clear function
+from os import system, name
+
+import pprint
+
+# used to represent coordinates, i.e. entity position, room location. due to how draw function works, (0,0) is the top left corner of a grid, with right being +x and down being +y
 class Position:
     def __init__(self, X, Y):
         self.X = X
@@ -7,15 +13,25 @@ class Position:
     def __eq__(self, other):
         return ((self.X, self.Y) == (other.X, other.Y))
 
+
+# clears console 
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
 def move():
     plyrMove = input()
-    if plyrMove == 'right' and 0 <= plyrPos.X <= rmSize - 2:
+    if plyrMove == 'right' and 0 <= plyrPos.X <= rmSize - 2 and wallV[plyrPos.Y][plyrPos.X] == 0:
         plyrPos.X += 1
-    elif plyrMove == 'left' and 1 <= plyrPos.X <= rmSize - 1:
+    elif plyrMove == 'left' and 1 <= plyrPos.X <= rmSize - 1 and wallV[plyrPos.Y][plyrPos.X - 1] == 0:
         plyrPos.X -= 1
-    elif plyrMove == 'down' and 0 <= plyrPos.Y <= rmSize - 2:
+    elif plyrMove == 'down' and 0 <= plyrPos.Y <= rmSize - 2 and wallH[plyrPos.Y][plyrPos.X] == 0:
         plyrPos.Y += 1
-    elif plyrMove == 'up' and 1 <= plyrPos.Y <= rmSize - 1:
+    elif plyrMove == 'up' and 1 <= plyrPos.Y <= rmSize - 1 and wallH[plyrPos.Y - 1][plyrPos.X] == 0:
         plyrPos.Y -= 1
 
 def rmDraw():
@@ -27,20 +43,41 @@ def rmDraw():
         print('||', end='')
         for x in range(rmSize):
             if rmPos == plyrPos:
-                print(' # ||', end='')
+                print(' # ', end='')
             else:
-                print('   ||', end='')
+                print('   ', end='')
+            if rmPos.X < rmSize - 1:
+                if wallV[rmPos.Y][rmPos.X] == 0:
+                    print('  ', end='')
+                else:
+                    print('||', end='')
             rmPos.X += 1
-        print('')
+        print('||\n==', end='')
+        wallHcheck = 0
         for x in range(rmSize):
-            print('=====', end='')
-        print('==')
+            if rmPos.Y < rmSize - 1:
+                if wallH[rmPos.Y][wallHcheck] == 0:
+                    print('   ', end='')
+                else:
+                    print('===', end='')
+                wallHcheck += 1
+            else:
+                print('===', end='')
+            print('==', end='')
+        print('')
         rmPos.X = 0
         rmPos.Y += 1
 
-plyrPos = Position(1, 1)
-rmSize = 4
+plyrPos = Position(0, 0)
+rmSize = 5
 
-while True:
-    rmDraw()
-    move()
+wallH = [[0, 1, 1, 0, 0], [1, 0, 0, 1, 0], [0, 1, 1, 0, 0], [0, 0, 0, 0, 0],]
+wallV = [[1, 0, 0, 0], [0, 1, 0, 1], [0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
+
+def game():
+    while True:
+        rmDraw()
+        move()
+        clear()
+
+game()
